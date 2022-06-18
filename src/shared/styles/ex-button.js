34cebb8a938336d9df5cf08d9c7bac831274f74a
yanglin <ss880309@gmail.com>
2022-06-18 20:37:46 +0800

    新增button

---
+++ src/shared/styles/ex-button.js
@@ -0,0 +1,37 @@
+import styled from "@emotion/styled";
+
+export const Button1 = styled.button`
+  padding: 32px;
+  background-color: hotpink;
+  font-size: 24px;
+  border-radius: 4px;
+  color: black;
+  font-weight: bold;
+  &:hover {
+    color: white;
+  }
+`
+
+export const Button2 = styled.button`
+  padding: 32px;
+  background-color: hotpink;
+  font-size: 24px;
+  border-radius: 4px;
+  color: black;
+  font-weight: bold;
+  &:hover {
+    color: white;
+  }
+`
+
+export const Button3 = styled.button`
+  padding: 32px;
+  background-color: hotpink;
+  font-size: 24px;
+  border-radius: 4px;
+  color: black;
+  font-weight: bold;
+  &:hover {
+    color: white;
+  }
+`
